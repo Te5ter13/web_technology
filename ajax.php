9d7755ae470040f1cef8e100c7ce66d7e8597d27
Manjil <94514259+t3st3r1E@users.noreply.github.com>
2022-01-06 10:34:22 +0545

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -17,13 +17,13 @@ function showHint(str) {
   }
 }
 </script>
-</head>
-<body>
-  <p><b>Start typing a name in the input field below:</b></p>
-  <form action="">
-    <label for="fname">First name:</label>
-    <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
-  </form>
-  <p>Suggestions: <span id="txtHint"></span></p>
-</body>
+  </head>
+  <body>    
+    <p><b>Start typing a name in the input field below:</b></p>
+    <form action="">
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+    </form>
+    <p>Suggestions: <span id="txtHint"></span></p>
+  </body>
 </html> 
